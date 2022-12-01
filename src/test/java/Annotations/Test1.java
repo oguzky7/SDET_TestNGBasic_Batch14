3a2cfefba57fc558b32303f6e7f114c249baab2e
oguzky7 <oguzky7@gmail.com>
2022-12-01 11:53:38 -0500

    12/1/2022

---
+++ src/test/java/Annotations/Test1.java
@@ -0,0 +1,10 @@
+package Annotations;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+    public void Test1(){
+        System.out.println("I'm Test1");
+    }
+}
---
+++ src/test/java/Annotations/Test2.java
@@ -0,0 +1,10 @@
+package Annotations;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+    @Test
+    public void Test2(){
+        System.out.println("I'm Test2");
+    }
+}
